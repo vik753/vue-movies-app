fd836827f48e80dafd5c105d9be390f7faa815db
Igor Korenets <vik753@gmail.com>
2020-03-31 16:40:50 +0300

    build dist

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  // devServer: {
+  //   overlay: {
+  //     warnings: true,
+  //     errors: false,
+  //   },
+  // },
+  // lintOnSave: process.env.NODE_ENV !== 'production',
+};
